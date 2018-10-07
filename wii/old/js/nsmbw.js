232bc6c57c9a3d680e65ce406576b730223fa144
RoadrunnerWMC <RoadrunnerWMC@users.noreply.github.com>
2018-10-06 17:36:50 -0700

    Initial commit

---
+++ wii/old/js/nsmbw.js
@@ -0,0 +1,8 @@
+    Cufon.replace('#logo', { fontFamily: 'TitilliumText22L-Thin',textShadow: '0 -1px 0 #ffffff' });
+    Cufon.replace('', { fontFamily: 'TitilliumText22L-Light' });
+    Cufon.replace('#banner #texts p, #features ul p, #legality', { fontFamily: 'TitilliumText22L-Regular' });
+    Cufon.replace('#features h3,#mailtoad h3, #boxes h4, #gallery h2, #seealso h3', { fontFamily: 'TitilliumText22L-Medium' });
+    Cufon.replace('', { fontFamily: 'TitilliumText22L-Bold' });
+    Cufon.replace('#logo strong, #banner #texts h2, .button span span, #watch-the-video h2, #features h2, #boxes h4, #mailtoad h2, #gallery h2, #seealso h3',  { fontFamily: 'TitilliumText22L-XBold' });
+    Cufon.replace('#specials h3', { fontFamily: 'SuperMarioBrosWii' })
+
